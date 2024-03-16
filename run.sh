ca7c231f661c8fee737d0f4f36b61f35108d4729
Jack~D <31171502+JackTildeD@users.noreply.github.com>
2024-03-15 21:18:19 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-# ---------------------------------------------------------------------------- #
+# -*- coding:utf-8 -*-
+
+# --- 80 cols ---------------------------------------------------------------- #
+
 SCRIPT_PATH="$(dirname $(readlink --canonicalize --no-newline "${0}"))"
 stow --dir="${SCRIPT_PATH}" --target="${HOME}" --verbose */
+
 # END OF LINE
